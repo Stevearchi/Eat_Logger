f19cf1a1205a9b39f37a94f75d133bfba1ab3ce5
Steve Archibald <stevearchi21@gmail.com>
2019-07-15 12:52:30 -0600

    still testing

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,16 @@ var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
   var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "root",
+    database: "hamburger_db"
+  });
 }
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "root",
-  database: "hamburger_db"
-});
 
 // Make connection.
 connection.connect(function(err) {
